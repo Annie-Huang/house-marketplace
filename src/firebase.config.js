589eb2bb964748f267e8dcaaf8473eee2aa7b0ed
Annie Huang <annie.hn@gmail.com>
2022-04-03 16:17:29 +1000

    Copy the instruction of "Then, initialise Firebase and begin using the SDKs for the products that you'd like to use." from https://console.firebase.google.com/project/house-marketplace-app-4cb05/overview into firebase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCjwAOys_0Vn2Y8y1E8FUF2EeACS-u6RNc',
+  authDomain: 'house-marketplace-app-4cb05.firebaseapp.com',
+  projectId: 'house-marketplace-app-4cb05',
+  storageBucket: 'house-marketplace-app-4cb05.appspot.com',
+  messagingSenderId: '751431557543',
+  appId: '1:751431557543:web:72f9cffb51f38101815682',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
