83415977ed758a9aacae0e5ed2ca60207fc512c3
Annie Huang <annie.hn@gmail.com>
2022-04-05 21:25:06 +1000

    Create showPassword and formData local state for SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,17 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import { useState } from 'react';
+
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formData;
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Sign In</h1>
