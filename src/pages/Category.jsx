c7ac533bfe7b88ac87494b99e01f6e90ff918df9
Annie Huang <annie.hn@gmail.com>
2022-04-15 17:56:14 +1000

    Import all the lib for Category.jsx and setup all the local states variables

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,4 +1,23 @@
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+
 const Category = () => {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
   return <div>category</div>;
 };
 
