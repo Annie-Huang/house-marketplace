f73f864c19ef8b9b3946ad91a4754e9ade76e631
Annie Huang <annie.hn@gmail.com>
2022-04-15 22:02:53 +1000

    Copy Category.tsx into Offers.jsx

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,9 +1,94 @@
-const Offer = () => {
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
+
+const Offers = () => {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, 'listings');
+
+        // Create a query
+        // Old version (v8) has the syntax of firebase.get().where().orderBy()
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        );
+
+        // Execute query
+        const querySnap = await getDocs(q);
+
+        let listings = [];
+
+        querySnap.forEach((doc) => {
+          // console.log(doc.data());
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Could not fetch listings');
+      }
+    };
+
+    fetchListings();
+  }, [params.categoryName]);
+
   return (
-    <div>
-      <h1>Offer</h1>
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Please for rent'
+            : 'Place for sale'}
+        </p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
+      )}
     </div>
   );
 };
 
-export default Offer;
+export default Offers;
