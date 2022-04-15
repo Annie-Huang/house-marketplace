59c66a31cf36947a29d47ff1f226523576137e51
Annie Huang <annie.hn@gmail.com>
2022-04-15 15:57:45 +1000

    Create the basic function and state for ForgotPassword.jsx page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,4 +1,18 @@
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const ForgotPassword = () => {
+  const [email, setEmail] = useState('');
+
+  const onChange = (e) => {};
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h1>Forgot Password</h1>
