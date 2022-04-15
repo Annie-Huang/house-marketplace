cc195095b9512a2efe200ca97c1ee2337abb5a44
Annie Huang <annie.hn@gmail.com>
2022-04-15 17:15:28 +1000

    Create the template for OAuth.jsx

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,5 +1,24 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import googleIcon from '../assets/svg/googleIcon.svg';
+
 const OAuth = () => {
-  return <div>oauth</div>;
+  const navigation = useNavigate();
+  const location = useLocation();
+
+  const onGoogleClick = (e) => {};
+
+  return (
+    <div className='socialLogin'>
+      <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with</p>
+      <button className='socialIconDiv' onClick={onGoogleClick}>
+        <img className='socialIconImg' src={googleIcon} alt='google' />
+      </button>
+    </div>
+  );
 };
 
 export default OAuth;
