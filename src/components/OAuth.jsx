fe22d38e2f24e13bed95a6a8df58fa834cb7f670
Annie Huang <annie.hn@gmail.com>
2022-04-15 17:26:39 +1000

    Add the logic of onGoogleClick, basically let user login with google provider, then check if the user exists. If not, create an users in the firebase store.

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,15 +1,38 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import googleIcon from '../assets/svg/googleIcon.svg';
 
 const OAuth = () => {
-  const navigation = useNavigate();
+  const navigate = useNavigate();
   const location = useLocation();
 
-  const onGoogleClick = (e) => {};
+  const onGoogleClick = async (e) => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check for user
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+
+      // If user doesn't exist, create user
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, 'users', user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate('/');
+    } catch (e) {
+      toast.error('Could not authorize with Google');
+    }
+  };
 
   return (
     <div className='socialLogin'>
