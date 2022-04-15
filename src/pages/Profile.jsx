e78cc60a0889023ed896b77e2b394aaead319ee8
Annie Huang <annie.hn@gmail.com>
2022-04-15 11:56:30 +1000

    How the main text in Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,6 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { updateDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
 import { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
@@ -15,10 +17,13 @@ import { useNavigate, Link } from 'react-router-dom';
 
 const Profile = () => {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
+
+  const { name, email } = formData;
 
   const navigate = useNavigate();
 
@@ -35,6 +40,15 @@ const Profile = () => {
           Logout
         </button>
       </header>
+
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p className='changePersonalDetails'>
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+      </main>
     </div>
   );
 };
