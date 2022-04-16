6be8e34671b6e9997568cdafa5037e1b516ad67b
Annie Huang <annie.hn@gmail.com>
2022-04-16 15:21:27 +1000

    Move the credential key to .env and reference it in CreateListing.jsx

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -79,7 +79,7 @@ const CreateListing = () => {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBM2bCgJ5IjAC3vVmKEppGhUbHqYbEoP14`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       );
 
       const data = await response.json();
@@ -102,6 +102,8 @@ const CreateListing = () => {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
       location = address;
+
+      console.log(geolocation, location);
     }
 
     setLoading(false);
