274ac0b4658060153a67568b7191b3ba950e4eb6
Annie Huang <annie.hn@gmail.com>
2022-04-17 21:22:55 +1000

    Add Swiper into Slider.jsx, Create onClick function to navigate to the listing

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -53,6 +53,23 @@ const Slider = () => {
     listings && (
       <>
         <p className='exploreHeading'>Recommended</p>
+
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                style={{
+                  background: `url(${data.imgUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='swiperSlideDiv'
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </>
     )
   );
