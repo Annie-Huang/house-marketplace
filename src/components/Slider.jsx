5da161442da529546f173439f16ffd9afc30c7bc
Annie Huang <annie.hn@gmail.com>
2022-04-17 16:58:53 +1000

    Add all the imports for Slider.jsx file

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import Spinner from './Spinner';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const Slider = () => {
   return <div>Slider</div>;
