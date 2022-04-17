68d00b66ca7fc9bf56aadb6407647e30148ae5c2
Annie Huang <annie.hn@gmail.com>
2022-04-17 13:21:37 +1000

    Create all local states in Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,18 @@
+import { useParams, useSearchParams } from 'react-router-dom';
+import { getDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import { useState } from 'react';
+
 const Contact = () => {
+  const [message, setMessage] = useState('');
+  const [landlord, setLandlord] = useState(null);
+
+  // https://reactrouter.com/docs/en/v6/api#usesearchparams
+  const [searchParam, setSearchParams] = useSearchParams();
+
+  const params = useParams();
+
   return <div>Contact</div>;
 };
 
