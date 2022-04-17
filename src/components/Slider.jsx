51546a49319f931edf2dd343d41dd2156262102e
Annie Huang <annie.hn@gmail.com>
2022-04-17 17:00:38 +1000

    Add all local states for Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,10 +5,16 @@ import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import Spinner from './Spinner';
+import { useState } from 'react';
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const Slider = () => {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
+  const navigate = useNavigate();
+
   return <div>Slider</div>;
 };
 
