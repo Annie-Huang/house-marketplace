a500718b941ec36d418651bf4ebb649c3246e6f5
Annie Huang <annie.hn@gmail.com>
2022-04-17 11:42:30 +1000

    Call fetchListing to firestore database in useEffect

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,12 +4,31 @@ import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [shareLinkCopied, setShareLinkCopied] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listings', params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [navigate, params.listingId]);
 
   return <div>LISTING</div>;
 };
