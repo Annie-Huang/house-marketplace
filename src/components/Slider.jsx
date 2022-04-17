6a7e050594c0a3032957526e4185ad3af04ef347
Annie Huang <annie.hn@gmail.com>
2022-04-17 21:02:45 +1000

    Create a fetchListings function in useEffect and make calls to firestore database to get a list of records stored there in Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,18 @@
 import { useNavigate } from 'react-router-dom';
-import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  where,
+} from 'firebase/firestore';
 import { db } from '../firebase.config';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import Spinner from './Spinner';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
@@ -14,6 +21,28 @@ const Slider = () => {
   const [listings, setListings] = useState(null);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingsRef = collection(db, 'listings');
+      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5));
+      const querySnap = await getDocs(q);
+
+      let listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, []);
 
   return <div>Slider</div>;
 };
