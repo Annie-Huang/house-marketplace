cb8624718dffff3816a8c2548bce362ce22dc71b
Annie Huang <annie.hn@gmail.com>
2022-04-17 10:29:36 +1000

    Create all locate states for Listing.jsx

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,4 +1,16 @@
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { getDoc, doc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { db } from '../firebase.config';
+import Spinner from '../components/Spinner';
+import shareIcon from '../assets/svg/shareIcon.svg';
+import { useState } from 'react';
+
 const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [shareLinkCopied, setShareLinkCopied] = useState(null);
+
   return <div>LISTING</div>;
 };
 
