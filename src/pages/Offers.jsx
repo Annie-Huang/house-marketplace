c60f7a34ffdb176a657caea0c39a3a13a0a83d8b
Annie Huang <annie.hn@gmail.com>
2022-04-18 12:30:16 +1000

    In Offer page, did exactly the same, copy lastFetchedListing, lastVisible, onFetchMoreListings across

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -5,6 +5,7 @@ import {
   where,
   orderBy,
   limit,
+  startAfter,
 } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
@@ -15,6 +16,7 @@ import ListingItem from '../components/ListingItem';
 const Offers = () => {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -36,6 +38,9 @@ const Offers = () => {
 
         let listings = [];
 
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+
         querySnap.forEach((doc) => {
           // console.log(doc.data());
           return listings.push({
@@ -53,6 +58,46 @@ const Offers = () => {
 
     fetchListings();
   }, []);
+
+  // Pagination / Load More
+  // Change fetchListings to limit(1) if you want to test onFetchMoreList
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, 'listings');
+
+      // Create a query
+      // Old version (v8) has the syntax of firebase.get().where().orderBy()
+      const q = query(
+        listingsRef,
+        where('type', '==', params.categoryName),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+
+      // Execute query
+      const querySnap = await getDocs(q);
+
+      let listings = [];
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      querySnap.forEach((doc) => {
+        console.log(doc.data());
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error('Could not fetch listings');
+    }
+  };
 
   return (
     <div className='category'>
